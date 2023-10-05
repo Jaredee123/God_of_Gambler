4557e652f265a346285f41bf2cc9be4e777f6dba
Jared Ee <jaredee2003@gmail.com>
2023-10-05 15:50:48 +0800

    updated app to GodofGambler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,66 @@
-
+import React, { useState } from 'react';
 
 function App() {
+  const [player_count, setplayer_count] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Inputs player_count={player_count} setplayer_count={setplayer_count} />
+        <Players player_count={player_count} />
+      </main>
+    </>
+  );
+}
+
+function Header() {
+  const appTitle = 'God of Gambler';
+  return (
+    <header>
+      <div className="logo">
+        <img
+          src="godofgambler_icon.png"
+          height="68"
+          width="68"
+          alt="God of Gambling logo"
+        />
+        <h1>{appTitle}</h1>
+      </div>
+    </header>
+  );
+}
+
+function Inputs({ player_count, setplayer_count }) {
+  return (
+    <form>
+      <input
+        type="number"
+        placeholder="Number of Players"
+        value={player_count}
+        onChange={(e) => setplayer_count(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function Players({ player_count }) {
+  function generate_player_inputs() {
+    const player_inputs = [];
+    for (let i = 1; i <= player_count; i++) {
+      player_inputs.push(
+        <input key={i} type="number" placeholder={`Player ${i}`} />
+      );
+    }
+    return player_inputs;
+  }
+  return (
+    <form>
+      <input type="number" placeholder="Buy In Amount" />
+      {generate_player_inputs()}
+    </form>
   );
 }
 
 export default App;
+
